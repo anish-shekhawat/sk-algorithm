7aa2fd6c6ab241cd9096a6548a2293af94725608
anish-shekhawat <anish.s.shekhawat@gmail.com>
2017-10-10 18:16:25 -0700

    Initial Commit

---
+++ sk_train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+""" Implements S-K Algorithm for training SVMs on Zener Cards. """
+
+
+def polynomial_kernal(vector_a, vecotr_b):
+    """Computes a degree four polynomial kernel function
+
+    :param a:
+    :param b:
+    :returns:
+    :raises:
+
+    """
+
